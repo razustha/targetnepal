79d1890101a8374ecef04ebe194dbb2117c6f4fd
razustha <razustha@hotmail.com>
2023-03-28 08:56:50 +0545

    add date in homepage job demand

--- page-templates/all-job.php
+++ page-templates/all-job.php
@@ -8,52 +8,124 @@
 
 
         <div class="row g-4">
-        <div class="row g-4">
-        <div class="col-md-3">
-                    <a href="assets/img/demand/tchrpvl_12_chait_20.png" data-lightbox="image-1">
-                        <img class="img-fluid rounded" src="assets/img/demand/tchrpvl_12_chait_20.png" alt="">
-                    </a>
-                </div>
+            <div class="row g-4">
             <div class="col-md-3">
+                    <a href="assets/img/demand/tchrpvl_12_chait_20.png" data-lightbox="image-1">
+                        <div class="card mb-4">
+
+                            <img class="img-fluid rounded" src="assets/img/demand/tchrpvl_12_chait_20.png" alt="">
+
+                            <div class="card-body">
+                                <h5 class="card-title">Published Date: 2079/12/20</h5>
+
+                            </div>
+                        </div>
+                    </a>
+
+                </div>
+                <div class="col-md-3">
                     <a href="assets/img/demand/tchrpvl_12_chait_20_2.png" data-lightbox="image-1">
-                        <img class="img-fluid rounded" src="assets/img/demand/tchrpvl_12_chait_20_2.png" alt="">
+                        <div class="card mb-4">
+
+                            <img class="img-fluid rounded" src="assets/img/demand/tchrpvl_12_chait_20_2.png" alt="">
+
+                            <div class="card-body">
+                                <h5 class="card-title">Published Date: 2079/12/20</h5>
+
+                            </div>
+                        </div>
+
                     </a>
                 </div>
-                     <div class="col-md-3">
+                <div class="col-md-3">
                     <a href="assets/img/demand/target_17gate.png" data-lightbox="image-1">
-                        <img class="img-fluid rounded" src="assets/img/demand/target_17gate.png" alt="">
+                        <div class="card mb-4">
+
+                            <img class="img-fluid rounded" src="assets/img/demand/target_17gate.png" alt="">
+
+                            <div class="card-body">
+                                <h5 class="card-title">Published Date: 2079/12/17</h5>
+
+                            </div>
+                        </div>
+
                     </a>
                 </div>
                 <div class="col-md-3">
                     <a href="assets/img/demand/target_16gate.png" data-lightbox="image-1">
-                        <img class="img-fluid rounded" src="assets/img/demand/target_16gate.png" alt="">
+                        <div class="card mb-4">
+
+                            <img class="img-fluid rounded" src="assets/img/demand/target_16gate.png" alt="">
+
+                            <div class="card-body">
+                                <h5 class="card-title">Published Date: 2079/12/26</h5>
+
+                            </div>
+                        </div>
+
                     </a>
                 </div>
                 <div class="col-md-3">
                     <a href="assets/img/demand/target_16gate_2.png" data-lightbox="image-1">
-                        <img class="img-fluid rounded" src="assets/img/demand/target_16gate_2.png" alt="">
+                        <div class="card mb-4">
+
+                            <img class="img-fluid rounded" src="assets/img/demand/target_16gate_2.png" alt="">
+
+                            <div class="card-body">
+                                <h5 class="card-title">Published Date: 2079/12/16</h5>
+
+                            </div>
+                        </div>
+
                     </a>
                 </div>
                 <div class="col-md-3">
                     <a href="assets/img/demand/tchrp_5_chait_2.png" data-lightbox="image-1">
-                        <img class="img-fluid rounded" src="assets/img/demand/tchrp_5_chait_2.png" alt="">
+                        <div class="card mb-4">
+
+                            <img class="img-fluid rounded" src="assets/img/demand/tchrp_5_chait_2.png" alt="">
+
+                            <div class="card-body">
+                                <h5 class="card-title">Published Date: 2079/12/13</h5>
+
+                            </div>
+                        </div>
+
                     </a>
                 </div>
                 <div class="col-md-3">
                     <a href="assets/img/demand/tchrp_5_chait.png" data-lightbox="image-1">
-                        <img class="img-fluid rounded" src="assets/img/demand/tchrp_5_chait.png" alt="">
+                        <div class="card mb-4">
+
+                            <img class="img-fluid rounded" src="assets/img/demand/tchrp_5_chait.png" alt="">
+
+                            <div class="card-body">
+                                <h5 class="card-title">Published Date: 2079/12/13</h5>
+
+                            </div>
+                        </div>
+
                     </a>
                 </div>
                 <div class="col-md-3">
                     <a href="assets/img/demand/tchrp_3_chait.png" data-lightbox="image-1">
-                        <img class="img-fluid rounded" src="assets/img/demand/tchrp_3_chait.png" alt="">
+                        <div class="card mb-4">
+
+                            <img class="img-fluid rounded" src="assets/img/demand/tchrp_3_chait.png" alt="">
+
+                            <div class="card-body">
+                                <h5 class="card-title">Published Date: 2079/12/11</h5>
+
+                            </div>
+                        </div>
+
                     </a>
                 </div>
+    
 
 
 
-            </div>
-           
+            </div>   
         </div>
         <div class="mt-5">
             <a class="learn-more" href="job-demand.php">View More <i class="fa-solid fa-arrow-right ms-2"></i></a>
