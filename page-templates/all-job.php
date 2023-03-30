16c4b7313f12fbe308c5301b7a5990c0626cda25
razustha <razustha@hotmail.com>
2023-03-30 16:54:34 +0545

    add demand latest

--- page-templates/all-job.php
+++ page-templates/all-job.php
@@ -10,6 +10,20 @@
         <div class="row g-4">
             <div class="row g-4">
             <div class="col-md-3">
+                    <a href="assets/img/demand/tchrpl_16_chait-1.png" data-lightbox="image-1">
+                        <div class="card mb-4">
+
+                            <img class="img-fluid rounded" src="assets/img/demand/tchrpl_16_chait-1.png" alt="">
+
+                            <div class="card-body">
+                                <h5 class="card-title">Published Date: 2079/12/17</h5>
+
+                            </div>
+                        </div>
+                    </a>
+
+                </div>
+            <div class="col-md-3">
                     <a href="assets/img/demand/tchrpl_15_chait_16_2.png" data-lightbox="image-1">
                         <div class="card mb-4">
 
