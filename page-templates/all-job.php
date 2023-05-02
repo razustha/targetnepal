ef6f0fc5b9ecd4c38359ab4f2921ed84560c13e8
razustha <razustha@hotmail.com>
2023-05-02 17:23:21 +0545

    add deman 19

--- page-templates/all-job.php
+++ page-templates/all-job.php
@@ -9,6 +9,16 @@
 
         <div class="row g-4">
         <div class="col-md-3">
+                    <a href="assets/img/demand/tchrp_19_baisakh_2080.png" data-lightbox="image-1">
+                        <div class="card mb-4">
+                            <img class="img-fluid rounded" src="assets/img/demand/tchrp_19_baisakh_2080.png" alt="">
+                            <div class="card-body">
+                                <h5 class="card-title">Published Date: 2080/01/20</h5>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+        <div class="col-md-3">
                     <a href="assets/img/demand/tchrp_14_baisakh_2080_1.png" data-lightbox="image-1">
                         <div class="card mb-4">
                             <img class="img-fluid rounded" src="assets/img/demand/tchrp_14_baisakh_2080_1.png" alt="">
@@ -98,20 +108,7 @@
 
             </div>
 
-            <div class="col-md-3">
-                <a href="assets/img/demand/tchrp_3_baisakh_080_1.png" data-lightbox="image-1">
-                    <div class="card mb-4">
-
-                        <img class="img-fluid rounded" src="assets/img/demand/tchrp_3_baisakh_080_1.png" alt="">
-
-                        <div class="card-body">
-                            <h5 class="card-title">Published Date: 2080/01/4</h5>
-
-                        </div>
-                    </div>
-                </a>
-
-            </div>
+            
         </div>
         <div class="mt-5">
             <a class="learn-more" href="job-demand.php">View More <i class="fa-solid fa-arrow-right ms-2"></i></a>
