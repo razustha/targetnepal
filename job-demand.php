6d0c74e2b4554549b1120a915cc4da595071326c
razustha <razustha@hotmail.com>
2023-05-23 11:31:43 +0545

    add demand jestha 9

--- job-demand.php
+++ job-demand.php
@@ -18,6 +18,16 @@
 
             <div class="row g-4">
             <div class="col-md-3">
+                <a href="assets/img/demand/02/9.jpg" data-lightbox="image-1">
+                    <div class="card mb-4">
+                        <img class="img-fluid rounded" src="assets/img/demand/02/9.jpg" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">Published Date: 2080/02/9</h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-3">
                 <a href="assets/img/demand/02/8.jpg" data-lightbox="image-1">
                     <div class="card mb-4">
                         <img class="img-fluid rounded" src="assets/img/demand/02/8.jpg" alt="">
--- page-templates/all-job.php
+++ page-templates/all-job.php
@@ -4,10 +4,20 @@
             <p class="custom-text fw-bold fs-5">Willing to go Abroad? Find jobs here.</p>
             <p class="medium-text">Current Demands</p>
         </div>
-
-
-       
         <div class="row g-4">
+
+
+
+        <div class="col-md-3">
+                <a href="assets/img/demand/02/9.jpg" data-lightbox="image-1">
+                    <div class="card mb-4">
+                        <img class="img-fluid rounded" src="assets/img/demand/02/9.jpg" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">Published Date: 2080/02/9</h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
         <div class="col-md-3">
                 <a href="assets/img/demand/02/8.jpg" data-lightbox="image-1">
                     <div class="card mb-4">
@@ -78,16 +88,7 @@
                     </div>
                 </a>
             </div>
-            <div class="col-md-3">
-                <a href="assets/img/demand/tchrp_26_baisakh_2080_1.png" data-lightbox="image-1">
-                    <div class="card mb-4">
-                        <img class="img-fluid rounded" src="assets/img/demand/tchrp_26_baisakh_2080_1.png" alt="">
-                        <div class="card-body">
-                            <h5 class="card-title">Published Date: 2080/01/27</h5>
-                        </div>
-                    </div>
-                </a>
-            </div>
+            
            
             
             </div>
