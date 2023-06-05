bfd9c0ba84cfb0423a267b46b38216796fcd8455
razustha <razustha@hotmail.com>
2023-06-05 11:53:37 +0545

    add demand jestha 22

--- job-demand.php
+++ job-demand.php
@@ -18,6 +18,16 @@
 
             <div class="row g-4">
             <div class="col-md-3">
+                <a href="assets/img/demand/02/22.jpg" data-lightbox="image-1">
+                    <div class="card mb-4">
+                        <img class="img-fluid rounded" src="assets/img/demand/02/22.jpg" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">Published Date: 2080/02/22</h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-3">
                 <a href="assets/img/demand/02/20.jpg" data-lightbox="image-1">
                     <div class="card mb-4">
                         <img class="img-fluid rounded" src="assets/img/demand/02/20.jpg" alt="">
--- page-templates/all-job.php
+++ page-templates/all-job.php
@@ -5,6 +5,16 @@
             <p class="medium-text">Current Demands</p>
         </div>
         <div class="row g-4">
+        <div class="col-md-3">
+                <a href="assets/img/demand/02/22.jpg" data-lightbox="image-1">
+                    <div class="card mb-4">
+                        <img class="img-fluid rounded" src="assets/img/demand/02/22.jpg" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">Published Date: 2080/02/22</h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
             <div class="col-md-3">
                 <a href="assets/img/demand/02/20.jpg" data-lightbox="image-1">
                     <div class="card mb-4">
